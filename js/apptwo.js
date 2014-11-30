7a34947ec1402fef88eaed1c9d0d4503cbab3231
beccaf19 <becca.f.1907@gmail.com>
2014-11-29 18:11:46 -0700

    updated photo functionality

--- js/apptwo.js
+++ js/apptwo.js
@@ -2,33 +2,70 @@
 
 
 $(document).ready(function(){
-	var photosUrl = 'https://api.flickr.com/services/feeds/photos_public.gne?id=129665272@N05&lang=en-us&format=json&jsoncallback=?'; 
-	$.getJSON(photosUrl)
+
+function displayPhotoAlbum(photos) {	
+		//get photos album 
+		var htmlString ='';
+
+		var albumHTML = '<h3 class="col-md-12" col-sm-12 col-lg-12>' + photos[1].albumName + '</h3>';
+		//label album
+		$('#images').append(albumHTML);
+		//for each photo in the album
+
+		for (var idx = 0; idx < photos.length; idx++) {
+			//get image src
+			var imageSrc = 'https://farm' + photos[idx].farm + '.staticflickr.com/' + photos[idx].server
+			+ '/' + photos[idx].id + '_' + photos[idx].secret + '.jpg';
+			htmlString = '<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4"> <img class="img-responsive" src=' + imageSrc + ' alt="' + photos[idx].id + '"> </li>';
+			//append li element to ul
+			$('#images').append(htmlString);
+		}
+
+}
+
+	var apiKey = '769390df99785d3372c1709fa68e6edc';
+	var userId= '129665272@N05';
+	var albumUrl = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key='+ apiKey +'&user_id='+ userId +'&format=json&&jsoncallback=?';
+	var albums =[];
+	
+	$.getJSON(albumUrl)
 		.done(function(data) {
+    		$.each(data.photosets.photoset, function (idx, set) {
+				var setId = set.id;
+				var albumPhotosUrl = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=' + apiKey + '&photoset_id='+setId+ '&format=json&&jsoncallback=?';
+				var photoAlbum =[];
+				$.getJSON(albumPhotosUrl) 
+					.done(function(data){
+						$.each(data.photoset.photo, function(i, photo) {
+							photoAlbum.push({
+								albumName: set.title._content,
+								picName: photo.title._content,
+								id: photo.id,
+								secret: photo.secret,
+								server: photo.server,
+								farm: photo.farm
+							});
+						})
+						albums.push({
+    						name: set.title._content,
+    						description: set.description._content,
+    						id: set.id,
+    						photos: set.photos,
+    						photoAlbum: photoAlbum
+    					});
+    					displayPhotoAlbum(photoAlbum);
+					})
+					.fail (function(error){
+						console.log(error);
+					});
+			})  
+		})
+		.fail(function(error) {
+			console.log(error);
+		}); 
+	// end of json
+}); //end of document ready
 
-    	// Start putting together the HTML string
-    	var htmlString = "";
-    
-    	// Now start cycling through our array of Flickr photo details
-    	$.each(data.items, function(i,item){
-    		// I only want the ickle square thumbnails
-       	 	var sourceSquare = item.media.m;
-       	 	debugger;
-        
-        	// Here's where we piece together the HTML
-        	htmlString += '<li><a href="' + item.link + '" target="_blank">';
-        	htmlString += '<img title="' + item.title + '" src="' + sourceSquare;
-        	htmlString += '" alt="'; htmlString += item.title + '" />';
-        	htmlString += '</a></li>';
-    		}); // end of each call
-    
-    		// Pop our HTML in the #images DIV
-    		$('#images').html(htmlString);
-    
-    	// Close down the JSON function call
-	})
 
 
 
-
-})
