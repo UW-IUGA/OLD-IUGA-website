a553b257030cd23cc19045198e785f8917ae390b
mmmscott <mmmscott@uw.edu>
2014-12-01 11:12:58 -0800

    add comments

--- js/check.js
+++ js/check.js
@@ -1,5 +1,8 @@
 $(function() {
 	var url = window.location.href;
+
+	// if current URL is the URL of specific nav link
+	// give it new class
 	$("nav ul li a").each(function() {
 		if (url == (this.href)) {
 			$(this).addClass("active");
