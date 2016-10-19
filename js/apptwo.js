47cc54e2a9da74480fa15bf47aca19f8e56b7f8f
Alexander Bell-Towne <alexander@bell-towne.com>
2016-10-19 16:41:03 -0700

    new officers

--- js/apptwo.js
+++ js/apptwo.js
@@ -3,17 +3,17 @@
 
 $(document).ready(function(){
 
-function displayPhotoAlbum(photos) {	
+function displayPhotoAlbum(photos) {
 	var htmlString ='';
 
 	//build and append html for album p element
 	var albumId= photos[1].albumName.charAt(0);
-	var albumString = '<p class= "' + albumId  + '"> </p>'; 
+	var albumString = '<p class= "' + albumId  + '"> </p>';
 	$('#imageGallery').append(albumString);
 	//build and append html for album header element
 	var headerString = "<h3>" + photos[1].albumName + "</h3>";
 	$('.' + albumId).append(headerString);
-	
+
 	//build html string for each photo
 	var imgStrings ='';
 	for (var idx = 0; idx < photos.length; idx++) {
@@ -23,14 +23,14 @@ function displayPhotoAlbum(photos) {
 		var bigImageSrc = imageSrc.replace('_q', '_z');
 		//create img element
 		imgStrings = imgStrings + '<a href ="' + bigImageSrc + '"class="' + albumId + '"> <img class="img-responsive" src="' + imageSrc + '" alt="' + photos[idx].albumName + '"> </a>';
-		
+
 	};
-	//append all photo html strings to album 
+	//append all photo html strings to album
 	$('.' + albumId).append(imgStrings);
 	//intialize colorbox (image popup functionality) on each album
 	$('a.' + albumId).colorbox({
 		rel: albumId,
-		transition: 'elastic',	
+		transition: 'elastic',
 		preloading: true,
 		arrowKey: true,
 		closeButton: true,
@@ -42,14 +42,14 @@ function displayPhotoAlbum(photos) {
 	var userId= '129665272@N05';
 	var albumUrl = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key='+ apiKey +'&user_id='+ userId +'&format=json&&jsoncallback=?';
 	var albums =[];
-	
+
 	$.getJSON(albumUrl)
 		.done(function(data) {
     		$.each(data.photosets.photoset, function (idx, set) {
 				var setId = set.id;
 				var albumPhotosUrl = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=' + apiKey + '&photoset_id='+setId+ '&format=json&&jsoncallback=?';
 				var photoAlbum =[];
-				$.getJSON(albumPhotosUrl) 
+				$.getJSON(albumPhotosUrl)
 					.done(function(data){
 						$.each(data.photoset.photo, function(i, photo) {
 							photoAlbum.push({
@@ -73,11 +73,11 @@ function displayPhotoAlbum(photos) {
 					.fail (function(error){
 						console.log(error);
 					});
-			})  
+			})
 		})
 		.fail(function(error) {
 			console.log(error);
-		}); 
+		});
 	// end of json
 }); //end of document ready
 
