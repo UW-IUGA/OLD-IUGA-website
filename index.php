998c8a72db151307c329e94b0cc6b0f7ebcc82b1
Alexander Bell-Towne <alexander@bell-towne.com>
2016-10-20 19:02:12 -0700

    restructure and convert to shared php

--- index.php
+++ index.php
@@ -1,57 +1,36 @@
-<!doctype html>
-<html lang="en">
+<?php
+    include_once('shared.php');
+    shared_header('./', 'Home');
+?>
+    <main>
 
-<head>
-    <title>Home</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/main.css" />
-    <link rel="icon" href="img/logo.ico">
+        <div class="container">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.1/angular.min.js"></script>
-</head>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <img class="img-responsive" alt="IUGA officers" src="./assets/img/officers/2016/home-image.jpg">
+                </div>
+            </div>
 
-<body ng-app="EventsApp" ng-controller="EventsController">
-    <nav>
-        <a href="/"><img id="logo" src="./img/IUGA-Logotype.jpg"></a>
-        <ul id="big-nav">
-            <li><a href="/">home</a></li>
-            <li><a href="aboutus.php">about</a></li>
-            <li><a href="events.php">events</a></li>
-            <li><a href="photos.php">photos</a></li>
-        </ul>
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"><span class="glyphicon glyphicon-th-list"></span></button>
-    </nav>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <h2>IUGA calendar of Events</h2>
+                </div>
+            </div>
 
-    <div class="popup-nav">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe src="https://calendar.google.com/calendar/embed?src=uw.edu_qrq301mbep5mn6mhulk2sfbpnk%40group.calendar.google.com&ctz=America/Los_Angeles"
+                                class="embed-responsive-item"
+                                style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+                    </div>
+                </div>
+            </div>
 
-        <ul id="small-nav">
-            <li><a href="/">home</a></li>
-            <li><a href="aboutus.php">about</a></li>
-            <li><a href="events.php">events</a></li>
-            <li><a href="photos.php">photos</a></li>
-        </ul>
-
-    </div>
-
-    <section id="home">
-        <img id="home-photo" src="img/officers/2016/home-image.jpg">
-    </section>
-
-
-    <div class="container">
-        <div class="responsive-iframe-container">
-            <iframe src="https://calendar.google.com/calendar/embed?src=uw.edu_qrq301mbep5mn6mhulk2sfbpnk%40group.calendar.google.com&ctz=America/Los_Angeles" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
         </div>
-    </div>
 
-
-    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="js/page.js"></script>
-    <script src="js/moment.js"></script>
-    <script src="js/app.js"></script>
-
-</body>
-
-</html>
+    </main>
+<?php
+    shared_footer('./');
+?>
