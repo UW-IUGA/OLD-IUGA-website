a11fa1e0861d01549bd2226486ea8476aef0fd07
Alexander Bell-Towne <Alexander@Bell-Towne.com>
2017-01-20 15:18:21 -0800

    move cal

--- index.php
+++ index.php
@@ -12,22 +12,6 @@
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <h2>IUGA calendar of Events</h2>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <iframe src="https://calendar.google.com/calendar/embed?src=uw.edu_neh0b0p4hso7ot07e5bnbimq8k%40group.calendar.google.com&ctz=America/Los_Angeles"
-                                class="embed-responsive-item"
-                                style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-                    </div>
-                </div>
-            </div>
-
         </div>
 
     </main>
