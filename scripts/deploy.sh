43272a23cb6d2dc05e29db9970823cde7f4ed010
Alexander Bell-Towne <Alexander@Bell-Towne.com>
2017-02-15 00:49:54 -0800

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 echo ""
 echo ""
-echo "Deploying to Server: http://beta.iuga.info"
+echo "Deploying to Server: http://iuga.info"
 echo "You will need to input your password"
 echo ""
 echo ""
@@ -8,10 +8,10 @@ echo ""
 rsync -av \
 -e 'ssh -p 7822' \
 --exclude=.DS_Store \
-build/ iuga@iuga.info:~/www/beta/
+build/ iuga@iuga.info:~/www/
 
 echo ""
 echo ""
-echo "Deployed!! visit it at: http://beta.iuga.info"
+echo "Deployed!! visit it at: http://iuga.info"
 echo ""
 echo ""
