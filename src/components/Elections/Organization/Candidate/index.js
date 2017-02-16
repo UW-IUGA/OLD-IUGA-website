0778e65e70b8ac3058028a6f140b6815becf58a1
Alexander Bell-Towne <Alexander@Bell-Towne.com>
2017-02-15 16:18:09 -0800

    Alexbbt elections merge iuga winfo (#16)
    Update to allow multiple Organizations

---
+++ src/components/Elections/Organization/Candidate/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import classnames from 'classnames';
+
+import './style.css';
+
+export default class Candidate extends Component {
+	render() {
+		if (this.props.candidate === null) {
+			return null;
+		}
+
+		const { className } = this.props;
+		return (
+			<div className={classnames('Candidate', className)}>
+				<h2>{this.props.candidate.name}, {this.props.candidate.role}</h2>
+				<div className="statement">
+					<img
+						alt={this.props.candidate.name + " for " + this.props.candidate.role}
+						src={`/assets/election/${this.props.candidate.year}/${this.props.candidate.image}`}
+					/>
+					{
+						this.props.candidate.statement.split("\n").map((p, i) => (
+							<p key={i}>{p}</p>
+						))
+					}
+					{ this.props.candidate.video &&
+						(
+							<div className="embed-responsive embed-responsive-16by9">
+								<iframe className="embed-responsive-item" src={this.props.candidate.video} frameBorder="0" allowFullScreen></iframe>
+							</div>)
+					}
+				</div>
+			</div>
+		);
+	}
+}
